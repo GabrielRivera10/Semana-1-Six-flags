3b0e08af89ca4ba0557aff5b6af9a19a4885ea64
Gabriel Rivera <gabrielmachuca@comunidad.unam.mx>
2022-05-01 20:35:48 -0500

    correcion 2

--- dynamics/boletos.php
+++ dynamics/boletos.php
@@ -15,67 +15,71 @@ $direccion  =(isset($_POST["direccion"]) && $_POST["direccion"] != "") ?$_POST["
 $TipoDeBoleto =(isset($_POST["TipoDeBoleto"]) && $_POST["TipoDeBoleto"] != "") ?$_POST["TipoDeBoleto"] : "no especifico";
 $boletos     =(isset($_POST["boletos"]) && $_POST["boletos"] != "") ?$_POST["boletos"] : "no especifico";
     for($n=1; $n<=$boletos; $n++){
-        echo "<table border=1 cellpadding=25px>
-            <thead>
-                <tr>
-                    <th colspan='4'><h1>Six Flags</h1></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td><br><br>$nombre
-                    <td><br><br>$apellidos
-                    <td><br><br>$edad
-                    <td rowspan='3'>"; 
-                    switch($TipoDeBoleto){
-                        case "Boleto de un día": 
-                            echo "<img src=\"./PadreSuburbano.png\" width=\"300\">";
-                            break;
-                        case "Adventure Pass 2022": 
-                            echo "<img src=\"./Adventure.jpg\" width=\"300\">";
-                            break;
-                        case "Flash Pass": 
-                            echo "<img src=\"./Rapidom.jpg\" width=\"300\">";
-                            break;
-                        case "Ultimate Pass 2022": 
-                            echo "<img src=\"./ultimate.jpg\" width=\"300\">";
-                            break;
-                        case "Comprar Six Flags 2022": 
-                            echo "<img src=\"./ricachon.png\" width=\"300\">";
-                            break;
-                    }
-                    
-                    echo "</td>
-                </tr>
-                <tr>
-                    <td><br><br>$direccion
-                    <td><br><br>$boletos
-                    <td><br><br>$TipoDeBoleto
-                </tr>
-                <tr>
-                    <td colspan='3'>";
-                    switch($TipoDeBoleto){
-                        case "Boleto de un día": 
-                            echo "<strong><i>Padre suburbano</i></strong>";
-                            break;
-                        case "Adventure Pass 2022": 
-                            echo "<strong><i>La aventura te espera</i></strong>";
-                            break;
-                        case "Flash Pass": 
-                            echo "<strong><i>Fiaaaaaaaaaum</i></strong>";
-                            break;
-                        case "Ultimate Pass 2022": 
-                            echo "<strong><i>Todo lo ultimate es mejor</i></strong>";
-                            break;
-                        case "Comprar Six Flags 2022": 
-                            echo "<strong><i>Señor ricon</i></strong>";
-                            break;
-                    }
-                    
-                    echo "</td>
-                </tr>
-            </tbody>
-        </table>";
+        if($n!=16){
+            echo "<table border=1 cellpadding=25px>
+                <thead>
+                    <tr>
+                        <th colspan='4'><h1>Six Flags</h1></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><br><br>$nombre
+                        <td><br><br>$apellidos
+                        <td><br><br>$edad
+                        <td rowspan='3'>"; 
+                        switch($TipoDeBoleto){
+                            case "Boleto de un día": 
+                                echo "<img src=\"./PadreSuburbano.png\" width=\"300\">";
+                                break;
+                            case "Adventure Pass 2022": 
+                                echo "<img src=\"./Adventure.jpg\" width=\"300\">";
+                                break;
+                            case "Flash Pass": 
+                                echo "<img src=\"./Rapidom.jpg\" width=\"300\">";
+                                break;
+                            case "Ultimate Pass 2022": 
+                                echo "<img src=\"./ultimate.jpg\" width=\"300\">";
+                                break;
+                            case "Comprar Six Flags 2022": 
+                                echo "<img src=\"./ricachon.png\" width=\"300\">";
+                                break;
+                        }
+                        
+                        echo "</td>
+                    </tr>
+                    <tr>
+                        <td><br><br>$direccion
+                        <td><br><br>$boletos
+                        <td><br><br>$TipoDeBoleto
+                    </tr>
+                    <tr>
+                        <td colspan='3'>";
+                        switch($TipoDeBoleto){
+                            case "Boleto de un día": 
+                                echo "<strong><i>Padre suburbano</i></strong>";
+                                break;
+                            case "Adventure Pass 2022": 
+                                echo "<strong><i>La aventura te espera</i></strong>";
+                                break;
+                            case "Flash Pass": 
+                                echo "<strong><i>Fiaaaaaaaaaum</i></strong>";
+                                break;
+                            case "Ultimate Pass 2022": 
+                                echo "<strong><i>Todo lo ultimate es mejor</i></strong>";
+                                break;
+                            case "Comprar Six Flags 2022": 
+                                echo "<strong><i>Señor ricon</i></strong>";
+                                break;
+                        }
+                        
+                        echo "</td>
+                    </tr>
+                </tbody>
+            </table>";
+        }
+        else
+            $n=$boletos+1;
     }
     if($n>15){
         $n=$boletos-15;
